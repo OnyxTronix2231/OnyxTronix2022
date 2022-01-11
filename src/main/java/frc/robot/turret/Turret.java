e16f3540541a899bd902045b1934b5117db1e0fe
RoamGreenstein <94557491+RoamGreenstein@users.noreply.github.com>
2022-01-11 18:28:58 +0200

    Update Turret.java

--- src/main/java/frc/robot/turret/Turret.java
+++ src/main/java/frc/robot/turret/Turret.java
@@ -1,4 +1,17 @@
 package frc.robot.turret;
 
-public class Turret {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Turret extends SubsystemBase {
+    private TurretComponents turretComponents;
+
+    public Turret(TurretComponents turretComponents) {this.turretComponents = turretComponents;}
+
+    public void setspeed(double speed) {
+        turretComponents.getMasterMotor().set(speed);
+    }
+
+    public void stop(){
+        setspeed(0);
+    }
 }
