22bbbb5b2a718c2bc884c847626071eef352c877
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-01-11 18:51:08 +0200

    started components

--- src/main/java/frc/robot/turret/TurretComponentsBase.java
+++ src/main/java/frc/robot/turret/TurretComponentsBase.java
@@ -3,16 +3,18 @@ package frc.robot.turret;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import static frc.robot.turret.TurretConstants.*;
+
 public class TurretComponentsBase implements TurretComponents    {
 
     private WPI_TalonFX masterMotor;
 
     public TurretComponentsBase(){
-        masterMotor = new WPI_TalonFX(0);
+        masterMotor = new WPI_TalonFX(TURRET_MOTOR_PORT);
         masterMotor.configFactoryDefault();
     }
 
-    public WPI_TalonFX getMasterMotor(){
+    public WPI_TalonFX getMotor(){
         return masterMotor;
     }
 }
