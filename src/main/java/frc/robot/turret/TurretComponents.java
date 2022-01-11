68149b364d54cbd6b8a817aca22d152d66430175
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-01-11 19:38:41 +0200

    created turret frame

--- src/main/java/frc/robot/turret/TurretComponents.java
+++ src/main/java/frc/robot/turret/TurretComponents.java
@@ -1,11 +1,14 @@
 package frc.robot.turret;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import pid.CtreMotionMagicController;
+import sensors.counter.CtreEncoder;
 
 import java.beans.Encoder;
 
 public interface TurretComponents {
 
     WPI_TalonFX getMotor();
-    Encoder getEncoder();
+    CtreEncoder getEncoder();
+    CtreMotionMagicController getController();
 }
