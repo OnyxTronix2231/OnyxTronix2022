9c4feced3f218923a3355d7509c02f6f2688d086
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-01-12 18:51:28 +0200

    created move to angle

--- src/main/java/frc/robot/turret/commands/MoveToAngle.java
+++ src/main/java/frc/robot/turret/commands/MoveToAngle.java
@@ -1,5 +1,32 @@
 package frc.robot.turret.commands;
 
-public class MoveToAngle {
-    
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.turret.Turret;
+
+import java.util.function.DoubleSupplier;
+
+public class MoveToAngle extends CommandBase {
+
+    private Turret turret;
+    private DoubleSupplier angleSupplier;
+
+    public MoveToAngle(Turret turret, DoubleSupplier angleSupplier) {
+        this.turret = turret;
+        this.angleSupplier = angleSupplier;
+    }
+
+    @Override
+    public void initialize() {
+        turret.initMoveToDegree(angleSupplier.getAsDouble());
+    }
+
+    @Override
+    public void execute() {
+        turret.updateMoveToDegree(angleSupplier.getAsDouble());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        turret.stop();
+    }
 }
