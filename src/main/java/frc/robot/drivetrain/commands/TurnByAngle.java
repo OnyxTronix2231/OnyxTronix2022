9ae3179a2964093df09658e0c2800e16ae2006f2
Yonatan Rubinshtein <yantegush@gmail.com>
2022-01-15 19:10:18 +0200

    Added a constructor to turn by angle

--- src/main/java/frc/robot/drivetrain/commands/TurnByAngle.java
+++ src/main/java/frc/robot/drivetrain/commands/TurnByAngle.java
@@ -1,7 +1,18 @@
 package frc.robot.drivetrain.commands;
 
-public class TurnByAngle {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.drivetrain.DriveTrain;
+
+import java.util.function.DoubleSupplier;
+
+public class TurnByAngle extends CommandBase {
+
+    private final DriveTrain driveTrain;
+    private final DoubleSupplier angleSupplier;
 
 
-
+    public TurnByAngle(DriveTrain driveTrain, DoubleSupplier angleSupplier) {
+        this.driveTrain = driveTrain;
+        this.angleSupplier = angleSupplier;
+    }
 }
