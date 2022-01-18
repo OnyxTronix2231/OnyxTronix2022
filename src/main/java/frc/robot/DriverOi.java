a0f6c4f88d3dfed2af1fb236a504dacb603673ba
LOL-xt <89537632+LOL-xt@users.noreply.github.com>
2022-01-18 20:36:00 +0200

    added move-by-path command and drive-by-distance (in progress)

--- src/main/java/frc/robot/DriverOi.java
+++ src/main/java/frc/robot/DriverOi.java
@@ -1,6 +1,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.drivetrain.DriveTrain;
+import frc.robot.drivetrain.DriverDriveTrainOiBinders;
 
 import static frc.robot.Constants.DRIVE_JOYSTICK_PORT;
 
@@ -10,5 +14,11 @@ public class DriverOi {
 
     public DriverOi() {
         xboxController = new XboxController(DRIVE_JOYSTICK_PORT);
+    }
+
+    public DriverOi withDriveTrain(DriveTrain driveTrain){
+        Trigger button = new JoystickButton(xboxController, XboxController.Button.kA.value);
+        new DriverDriveTrainOiBinders(driveTrain, button);
+        return this;
     }
 }
