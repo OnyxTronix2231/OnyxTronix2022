8a96a2d0422ab1f4e0e8737ca2b3937b9ac4a0b1
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-01-19 22:13:48 +0200

    Fixed conventions

--- src/main/java/frc/robot/turret/DeputyTurretOiBinder.java
+++ src/main/java/frc/robot/turret/DeputyTurretOiBinder.java
@@ -7,7 +7,9 @@ import static frc.robot.turret.TurretConstants.*;
 public class DeputyTurretOiBinder {
 
     public DeputyTurretOiBinder(Turret turret, JoystickAxis moveLeft, JoystickAxis moveRight) {
-        moveLeft.whileActiveContinuous(new RotateTurretBySpeed(turret, () -> moveLeft.getRawAxis() * -ROTATION_SETBACK));
-        moveRight.whileActiveContinuous(new RotateTurretBySpeed(turret, () -> moveRight.getRawAxis() * ROTATION_SETBACK));
+        moveLeft.whileActiveContinuous(new RotateTurretBySpeed(turret,
+                () -> moveLeft.getRawAxis() * -ROTATION_SETBACK));
+        moveRight.whileActiveContinuous(new RotateTurretBySpeed(turret,
+                () -> moveRight.getRawAxis() * ROTATION_SETBACK));
     }
 }
