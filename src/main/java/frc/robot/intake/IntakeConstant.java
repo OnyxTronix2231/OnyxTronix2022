a3e3981132c6964b4df409134edc248a8bfc4f77
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-01-19 20:20:12 +0200

    nimi is the absolute goat
    yuval gur is nimi legacy

--- src/main/java/frc/robot/intake/IntakeConstant.java
+++ src/main/java/frc/robot/intake/IntakeConstant.java
@@ -7,7 +7,7 @@ public class IntakeConstant {
    public static final DoubleSolenoid.Value SOLENOID_OPEN = DoubleSolenoid.Value.kForward;
    public static final DoubleSolenoid.Value SOLENOID_CLOSE = DoubleSolenoid.Value.kReverse;
 
-   public static final int MOTOR_ID = 0;
-   public static final int SOLENOID_FORWARD_CHANNEL = 0;
-   public static final int SOLENOID_REVERSE_CHANNEL = 0;
+   public static final int MOTOR_ID = 9;
+   public static final int SOLENOID_FORWARD_CHANNEL = 1;
+   public static final int SOLENOID_REVERSE_CHANNEL = 2;
 }
