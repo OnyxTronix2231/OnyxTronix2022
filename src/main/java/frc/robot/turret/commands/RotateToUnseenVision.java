025344a91b00fe3bec746de159053daf5917451b
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-01-19 21:52:21 +0200

    conventions

--- src/main/java/frc/robot/turret/commands/RotateToUnseenVision.java
+++ src/main/java/frc/robot/turret/commands/RotateToUnseenVision.java
@@ -2,33 +2,33 @@ package frc.robot.turret.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.turret.Turret;
-
 import static frc.robot.turret.TurretConstants.*;
 
 public class RotateToUnseenVision extends CommandBase {
+
     private Turret turret;
     private CurrentGoal currentGoal;
 
-    public RotateToUnseenVision(Turret turret){
+    public RotateToUnseenVision(Turret turret) {
         this.turret = turret;
 
         double angle = turret.getCurrentAngle();
-        if (angle > 0 && angle <= (MIN_DEG + DEG_IN_ROTATION)){
+        if (angle > MIDDLE_ANGLE && angle <= (MIN_DEG + DEG_IN_ROTATION)) {
             currentGoal = CurrentGoal.goto180;
-        } else if (angle <= 0 && angle >= (MAX_DEG - DEG_IN_ROTATION)){
+        } else if (angle <= MIDDLE_ANGLE && angle >= (MAX_DEG - DEG_IN_ROTATION)) {
             currentGoal = CurrentGoal.goto180m;
-        } else if (angle >= (MIN_DEG + DEG_IN_ROTATION)){
+        } else if (angle >= (MIN_DEG + DEG_IN_ROTATION)) {
             currentGoal = CurrentGoal.gotoMin;
-        } else if (angle <= (MAX_DEG - DEG_IN_ROTATION)){
+        } else if (angle <= (MAX_DEG - DEG_IN_ROTATION)) {
             currentGoal = CurrentGoal.gotoMax;
         }
     }
 
     @Override
     public void initialize() {
-        switch (currentGoal){
-            case goto180: turret.initMoveToDegree(180);
-            case goto180m: turret.initMoveByDegree(-180);
+        switch (currentGoal) {
+            case goto180: turret.initMoveToDegree(HALF_ROTATION);
+            case goto180m: turret.initMoveByDegree(-HALF_ROTATION);
             case gotoMax: turret.initMoveByDegree(MAX_DEG);
             case gotoMin: turret.initMoveByDegree(MIN_DEG);
         }
@@ -36,31 +36,40 @@ public class RotateToUnseenVision extends CommandBase {
 
     @Override
     public void execute() {
-        switch (currentGoal){
+        switch (currentGoal) {
             case goto180:
-                if (Math.abs(turret.getCurrentAngle() - 180) <= TOLERANCE_DEGREE){
+                if (Math.abs(turret.getCurrentAngle() - HALF_ROTATION) <= TOLERANCE_DEGREE) {
                     currentGoal = CurrentGoal.gotoMin;
-                } else { turret.updateMoveToDegree(180);}
+                } else {
+                    turret.updateMoveToDegree(HALF_ROTATION);
+                }
 
             case goto180m:
-                if ((Math.abs(turret.getCurrentAngle()) + 180) <= TOLERANCE_DEGREE){
+                if ((Math.abs(turret.getCurrentAngle()) + HALF_ROTATION) <= TOLERANCE_DEGREE) {
                     currentGoal = CurrentGoal.gotoMax;
-                } else { turret.updateMoveByDegree(-180);}
+                } else {
+                    turret.updateMoveByDegree(-HALF_ROTATION);
+                }
 
             case gotoMax:
-                if (Math.abs(turret.getCurrentAngle() - MAX_DEG) <= TOLERANCE_DEGREE){
+                if (Math.abs(turret.getCurrentAngle() - MAX_DEG) <= TOLERANCE_DEGREE) {
                     currentGoal = CurrentGoal.gotoMin;
-                } else { turret.updateMoveToDegree(MAX_DEG);}
+                } else {
+                    turret.updateMoveToDegree(MAX_DEG);
+                }
 
             case gotoMin:
-                if (Math.abs(turret.getCurrentAngle() - MIN_DEG) <= TOLERANCE_DEGREE){
+                if (Math.abs(turret.getCurrentAngle() - MIN_DEG) <= TOLERANCE_DEGREE) {
                     currentGoal = CurrentGoal.gotoMax;
-                } else { turret.updateMoveToDegree(MIN_DEG);}
+                } else {
+                    turret.updateMoveToDegree(MIN_DEG);
+                }
         }
     }
 }
 
-enum CurrentGoal{
+enum CurrentGoal {
+
     goto180(1),
     goto180m(2),
     gotoMax(3),
@@ -68,7 +77,7 @@ enum CurrentGoal{
 
     final int value;
 
-    CurrentGoal(int value){
+    CurrentGoal(int value) {
         this.value = value;
     }
 }
