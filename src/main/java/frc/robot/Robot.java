157142e3e7f3fae023017dfae3217fca55c9c7bc
RozenX <94130292+RozenX@users.noreply.github.com>
2022-01-22 10:32:06 +0200

    wip

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.conveyor.Conveyor;
+import frc.robot.conveyor.ConveyorComponents;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
