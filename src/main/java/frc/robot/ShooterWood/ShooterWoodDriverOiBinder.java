313c08da912e4865f444ea4116de337d21396f9e
michalrobotics <michalhe07@gmail.com>
2022-01-22 11:52:17 +0200

    added shoot rpm

--- src/main/java/frc/robot/ShooterWood/ShooterWoodDriverOiBinder.java
+++ src/main/java/frc/robot/ShooterWood/ShooterWoodDriverOiBinder.java
@@ -1,6 +1,7 @@
 package frc.robot.ShooterWood;
 
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.ShooterWood.Commands.ShootByRPMWood;
 import frc.robot.ShooterWood.Commands.ShootBySpeedWood;
 
 import static frc.robot.ShooterWood.ShooterWoodConstants.SPEED;
@@ -8,6 +9,6 @@ import static frc.robot.ShooterWood.ShooterWoodConstants.SPEED;
         public class ShooterWoodDriverOiBinder {
 
             public ShooterWoodDriverOiBinder(ShooterWood shooterWood, Trigger shootBySpeed) {
-                shootBySpeed.whileActiveContinuous(new ShootBySpeedWood(shooterWood, () -> shooterWood.getSpeed()));
+                shootBySpeed.whileActiveContinuous(new ShootByRPMWood(shooterWood, () -> shooterWood.getSpeed()));
     }
 }
