3e2a000e5c8f6fe7d6720c57e205e1cb953a5c70
LOL-xt <89537632+LOL-xt@users.noreply.github.com>
2022-01-23 20:21:36 +0200

    added move-by-path command (in progress)!

--- src/main/java/frc/robot/drivetrain/DriverDriveTrainOiBinders.java
+++ src/main/java/frc/robot/drivetrain/DriverDriveTrainOiBinders.java
@@ -5,13 +5,16 @@ package frc.robot.drivetrain;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.drivetrain.commands.DriveByDistance;
 import frc.robot.drivetrain.commands.DriveBySpeed;
+import frc.robot.drivetrain.commands.MoveByPath;
 import onyxTronix.JoystickAxis;
+
+import static frc.robot.drivetrain.DriveTrainConstants.Paths.ONE_METER_FORWARD;
 
 public class DriverDriveTrainOiBinders {
 
     public DriverDriveTrainOiBinders(DriveTrain driveTrain, JoystickAxis rightJoystick, JoystickAxis leftJoystick, Trigger button){
         driveTrain.setDefaultCommand(new DriveBySpeed(driveTrain, rightJoystick::getRawAxis, leftJoystick::getRawAxis));
-        button.whenActive(new DriveByDistance(driveTrain, ()-> 1.0));
+        button.whenActive(new MoveByPath(driveTrain, ONE_METER_FORWARD));
     }
 
 
