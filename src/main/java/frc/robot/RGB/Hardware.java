42d8be03d630d49452f75af9e43abd73c37079ad
DanGoldenberg <yantegush@gmail.com>
2022-01-23 23:25:09 +0200

    fixed stuff
    I will add documentation, not now, I want to sleep.

--- src/main/java/frc/robot/RGB/Hardware.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.RGB;
-
-import com.ctre.phoenix.CANifier;
-
-public class Hardware {
-    public static CANifier canifier = new CANifier(0);
-}
--- src/main/java/frc/robot/RGB/OnyxRGB.java
+++ src/main/java/frc/robot/RGB/OnyxRGB.java
@@ -5,33 +5,53 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import java.util.Arrays;
 
-import static frc.robot.RGB.Constants.kMotorControllerCh;
+import static frc.robot.RGB.Constants.*;
 
-
+/**
+ * OnyxRGB is a class for an easy to control RGB led set.
+ *
+ * @since 2022-01-23
+ */
 public class OnyxRGB extends SubsystemBase {
-    private static float RGB[] = new float[3];
+    private static float[] RGB = new float[RGB_ARRAY_SIZE];
+    public static CANifier canifier;
     private final ColorNameToRGB colorNameToRGB;
 
-    public OnyxRGB(){
-        colorNameToRGB = new ColorNameToRGB();
-        Arrays.fill(RGB, 0);
-        Hardware.canifier.setPWMOutput(kMotorControllerCh.value, 0);
-        Hardware.canifier.enablePWMOutput(kMotorControllerCh.value, true);
-    }
+    public OnyxRGB(int deviceID){
+        canifier = new CANifier(deviceID);
+        canifier.setPWMOutput(kMotorControllerCh.value, DUTY_CYCLES);
+        canifier.enablePWMOutput(kMotorControllerCh.value, true);
 
+        colorNameToRGB = new ColorNameToRGB();
+        Arrays.fill(RGB, DEFAULT_COLOR_VALUE);
+    }
 
     @Override
     public void periodic(){
-        Hardware.canifier.setLEDOutput(RGB[0], CANifier.LEDChannel.LEDChannelA);
-        Hardware.canifier.setLEDOutput(RGB[1], CANifier.LEDChannel.LEDChannelB);
-        Hardware.canifier.setLEDOutput(RGB[2], CANifier.LEDChannel.LEDChannelC);
+        canifier.setLEDOutput(RGB[R], CANifier.LEDChannel.LEDChannelA);
+        canifier.setLEDOutput(RGB[G], CANifier.LEDChannel.LEDChannelB);
+        canifier.setLEDOutput(RGB[B], CANifier.LEDChannel.LEDChannelC);
     }
 
     public void onStop(){
-        Hardware.canifier.enablePWMOutput(Constants.kMotorControllerCh.value, false);
+        canifier.enablePWMOutput(kMotorControllerCh.value, false);
     }
 
-    public void setColor(Colors color){
-        RGB = colorNameToRGB.getColorNameToRGB(color);
+    public void setColorByName(Colors color){
+        RGB = colorNameToRGB.getColorNameToRGB(color).clone();
+    }
+
+    public void setColorByRGB(int r, int g, int b){
+        r = Math.max(MIN_RGB_VALUE, Math.min(MAX_RGB_VALUE, r));
+        g = Math.max(MIN_RGB_VALUE, Math.min(MAX_RGB_VALUE, g));
+        b = Math.max(MIN_RGB_VALUE, Math.min(MAX_RGB_VALUE, b));
+
+        RGB[R] = r;
+        RGB[G] = g;
+        RGB[B] = b;
+    }
+
+    public void turnOffRGB(){
+        setColorByName(Colors.Black);
     }
 }
