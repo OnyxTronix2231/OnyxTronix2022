5857bb88436c2ce38a530e1048b2d4376ff7b6c4
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-01-24 19:33:57 +0200

    fixed cr

--- src/main/java/frc/robot/turret/TurretConstants.java
+++ src/main/java/frc/robot/turret/TurretConstants.java
@@ -7,7 +7,7 @@ public final class TurretConstants {
 
     /*fixed values*/
     public static final int ENCODER_UNITS_IN_ROTATION = 4096; //TODO: confirm
-    public static final int DEG_IN_ROTATION = 360;
+    public static final int DEG_IN_CIRCLE = 360;
     public static final int HALF_ROTATION = 180;
     public static final Pose2d TARGET_POS = new Pose2d(0,0,new Rotation2d()); //TODO: confirm
 
@@ -17,6 +17,7 @@ public final class TurretConstants {
     public static final double MAX_DEG = 0; //TODO: Correct the number
     public static final double MIN_DEG = 0; //TODO: Correct the number
     public static final double TOLERANCE_DEGREE = 1; //TODO: Correct the number
+    public static final double DEG_IN_TURRET_ROTATION = CONVERSION_RATE * DEG_IN_CIRCLE;
 
     /*PID values*/
     public static final int KP = 0; //TODO: calibrate
--- src/main/java/frc/robot/yawControl/Commands/RotateByPos.java
+++ src/main/java/frc/robot/yawControl/Commands/RotateByPos.java
@@ -3,9 +3,9 @@ package frc.robot.yawControl.Commands;
 import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.yawControl.YawControl;
 
-public class RotateToPos extends RotateToAngleRTF {
+public class RotateByPos extends RotateToAngleRTF {
 
-    public RotateToPos(YawControl yawControl, Pose2d currentPos) {
+    public RotateByPos(YawControl yawControl, Pose2d currentPos) {
         super(yawControl, () -> yawControl.getAngleToTargetRTF(currentPos));
         addRequirements(yawControl);
     }
