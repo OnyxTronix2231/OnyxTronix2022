e5fad106819e09195fe917d0d31fe5799ae6fe47
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-01-29 21:25:13 +0200

    fixed the non vision dependent shooting algorithem, finished base calculations of the shooting while moving algorithems

--- src/main/java/frc/robot/crossPlatform/ShootWhileDrivingCalc.java
+++ src/main/java/frc/robot/crossPlatform/ShootWhileDrivingCalc.java
@@ -11,7 +11,8 @@ public class ShootWhileDrivingCalc {
     private final Arc arc;
     private final YawControl yawControl;
 
-    public ShootWhileDrivingCalc(DriveTrain drivetrain, Vision vision, Shooter shooter, Arc arc, YawControl yawControl) {
+    public ShootWhileDrivingCalc
+            (DriveTrain drivetrain, Vision vision, Shooter shooter, Arc arc, YawControl yawControl) {
         this.drivetrain = drivetrain;
         this.vision = vision;
         this.shooter = shooter;
@@ -19,30 +20,111 @@ public class ShootWhileDrivingCalc {
         this.yawControl = yawControl;
     }
 
-    public static Vector2d getShooterOnly2DVector() {
+    public Vector2d getShooterOnly2DVector() {
+        /*the vector that represents the first movement of the ball in the air (from the side)
 
-        return null;
+        robot   ^             hub
+               /            \     /
+           /\ /              \_ _/
+        __|_|                |   |
+        \---/                |   |
+        ______________________________floor
+
+        * size -> speed of the shooting system (m/s)
+        * angle -> arc's direction (deg)*/
+
+        double size = shooter.getNeededSpeed();
+        double angle = arc.getNeededAngle();
+        double x = size * Math.cos(Math.toRadians(angle));
+        double y = size * Math.sin(Math.toRadians(angle));
+        return new Vector2d(x, y);
     }
 
-    public static Vector2d getXZ2DVector() {
+    public Vector2d getXZ2DVector() {
+        /*the vector that represents the horizontal movement of the ball (from the top)
 
-        return null;
+        robot                 hub
+
+        /---\                _---_
+        |()-+--->           {     }
+        \---/                -___-
+
+        * size -> horizontal speed of shooting system (m/s)
+        * angle -> turret's direction*/
+
+        double size = getShooterOnly2DVector().x;
+        double angle = yawControl.getAngleNoVisionDependent(vision);
+        double x = size * Math.cos(Math.toRadians(angle));
+        double y = size * Math.sin(Math.toRadians(angle));
+        return new Vector2d(x, y);
     }
 
-    public static double getTurretFixedAngle() {
+    public Vector2d getMovementVector() {
+        /*the vector that represents the movement of the robot (from the top)
 
-        return 0;
+        robot                   hub
+                _->
+        /---\_--               _---_
+        |() |                 {     }
+        \---/                  -___-
+
+        * size -> speed of the robot (m/s)
+        * angle -> robot's direction*/
+
+        double size = drivetrain.getCorrectRobotSpeed();
+        double angle = drivetrain.getHedding();
+        double x = size * Math.cos(Math.toRadians(angle));
+        double y = size * Math.sin(Math.toRadians(angle));
+        return new Vector2d(x, y);
     }
 
-    public static double getShootertFixedSpeedMPS() {
+    public Vector2d getCorrectTrajectory() {
+        /*the vector that represents the fixed horizontal movement of the ball (from the top)
+        it is fixed according to the movement of the whole robot
 
-        return 0;
+        robot   ^               hub
+               /
+        /---\/                 _---_
+        |()-+                 {     }
+        \---/                  -___-
+
+        * size -> new & fixed horizontal speed of shooting system (m/s)
+        * angle -> new & fixed turret's direction*/
+
+        Vector2d turretVector = getXZ2DVector();
+        Vector2d movementVector = getMovementVector();
+        return new Vector2d(turretVector.x - movementVector.x, turretVector.y - movementVector.y);
     }
 
-    public static double getShootertFixedAngle() {
+    public Vector2d getFixedShootingVector() {
+        /*the vector that represents the fixed movement of the ball in the air (from the side)
+        it is fixed by the changed horizontal vector
 
-        return 0;
+        robot   ^             hub
+               /            \     /
+           /\ /              \_ _/
+        __|_|                |   |
+        \---/                |   |
+        ______________________________floor
+
+        * size -> fixed speed of the shooting system (m/s)
+        * angle -> fixed arc's direction (deg)*/
+        double x = getCorrectTrajectory().magnitude();
+        double y = getShooterOnly2DVector().y;
+        return new Vector2d(x, y);
     }
 
+    public double getTurretFixedAngle() {
+        Vector2d correctVector = getCorrectTrajectory();
+        return Math.toDegrees(Math.atan(correctVector.y / correctVector.x));
+    }
 
+    public double getShooterFixedSpeedMPS() {
+        return getFixedShootingVector().magnitude();
+    }
+
+    public double getShooterFixedAngle() {
+        Vector2d correctVector = getFixedShootingVector();
+        return Math.toDegrees(Math.atan(correctVector.y / correctVector.x));
+    }
 }
