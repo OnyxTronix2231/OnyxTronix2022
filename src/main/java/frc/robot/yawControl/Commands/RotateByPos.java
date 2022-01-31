37864169a805c4359dbbcebb18678d35aa8d040e
shaharbetser <shaharbetser8@gmail.com>
2022-01-31 21:34:30 +0200

    Fixing little things

--- src/main/java/frc/robot/yawControl/Commands/RotateByPos.java
+++ src/main/java/frc/robot/yawControl/Commands/RotateByPos.java
@@ -4,9 +4,9 @@ import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.yawControl.Commands.RotateToAngleRTF;
 import frc.robot.yawControl.YawControl;
 
-public class RotateByPose extends RotateToAngleRTF {
+public class RotateByPos extends RotateToAngleRTF {
 
-    public RotateByPose(YawControl yawControl, Pose2d currentPos) {
+    public RotateByPos(YawControl yawControl, Pose2d currentPos) {
         super(yawControl, () -> yawControl.getAngleToTargetRTF(currentPos));
         super(yawControl, () -> yawControl.getAngleToTargetByPose(currentPos));
         addRequirements(yawControl);
