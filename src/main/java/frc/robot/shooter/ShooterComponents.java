ff7851d423fe5d3fdc15da19ca894e2e1c93f91b
michalrobotics <michalhe07@gmail.com>
2022-02-01 21:42:06 +0200

    deleted shooter wood

--- src/main/java/frc/robot/shooter/ShooterComponents.java
+++ src/main/java/frc/robot/shooter/ShooterComponents.java
@@ -1,8 +1,6 @@
 package frc.robot.shooter;
 
-import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import pid.interfaces.PIDController;
 import sensors.counter.Counter;
 
