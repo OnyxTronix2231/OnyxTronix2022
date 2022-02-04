42a9833bd240527147887e07d1aee86bb437a999
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-02-05 00:09:25 +0200

    merge with develop

--- src/main/java/frc/robot/DriverOi.java
+++ src/main/java/frc/robot/DriverOi.java
@@ -1,8 +1,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.drivetrain.DriveTrain;
 import frc.robot.drivetrain.DriverDriveTrainOiBinders;
+import frc.robot.trigger.TriggerOiBinder;
+import frc.robot.trigger.TriggerSub;
 import onyxTronix.JoystickAxis;
 
 import static frc.robot.Constants.DRIVE_JOYSTICK_PORT;
@@ -19,6 +22,12 @@ public class DriverOi {
         JoystickAxis leftJoystick = new JoystickAxis(xboxController, XboxController.Axis.kLeftY.value);
         JoystickAxis rightJoystick = new JoystickAxis(xboxController, XboxController.Axis.kRightX.value);
         new DriverDriveTrainOiBinders(driveTrain, leftJoystick, rightJoystick);
+        return this;
+    }
+
+    public DriverOi triggerTest(TriggerSub trigger) {
+        JoystickButton triggerMoveBySpeed = new JoystickButton(xboxController, XboxController.Button.kA.value);
+        new TriggerOiBinder(trigger, triggerMoveBySpeed);
         return this;
     }
 }
