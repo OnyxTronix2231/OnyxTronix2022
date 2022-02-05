e9e5dc82758600266256d5dc076ecd690fefdce2
DanGoldenberg <yantegush@gmail.com>
2022-02-05 15:42:14 +0200

    WE DID visiom

--- src/main/java/frc/robot/vision/VisionConstants.java
+++ src/main/java/frc/robot/vision/VisionConstants.java
@@ -5,7 +5,6 @@ public final class VisionConstants {
     public static final double OUTER_TARGET_HEIGHT_CM = 153;
     public static final double LIMELIGHT_HEIGHT_TO_FLOOR_CM = 59.3;
     public static final double LIMELIGHT_ANGLE_TO_HORIZON_DEG = 17.6;
-    public static final double LIMELIGHT_TO_TARGET_CM =
-            OUTER_TARGET_HEIGHT_CM - LIMELIGHT_HEIGHT_TO_FLOOR_CM;
+    public static final double LIMELIGHT_TO_TARGET_CM = OUTER_TARGET_HEIGHT_CM - LIMELIGHT_HEIGHT_TO_FLOOR_CM;
     public static final double LIMELIGHT_TO_TURRET_CM = 15.195728859;
 }
