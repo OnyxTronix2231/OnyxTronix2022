0c2e6045d9cc545c1a36ba796c5d285396691755
YuvalGur135 <75333129+YuvalGur135@users.noreply.github.com>
2022-02-06 19:12:39 +0200

    little fix

--- src/main/java/frc/robot/yawControl/DriverYawControlOiBinder.java
+++ src/main/java/frc/robot/yawControl/DriverYawControlOiBinder.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.yawControl.Commands.RotateToAngleRTF;
 
 public class DriverYawControlOiBinder extends CommandBase {
-    
+
     public DriverYawControlOiBinder(YawControl yawControl, Trigger rotateAngleRTF){
         rotateAngleRTF.toggleWhenActive(new RotateToAngleRTF(yawControl, ()-> 0));
     }
