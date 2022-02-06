3332000d01e41f7f339108150353499a28a7e4ae
yuvalys <yuvaly.segev@gmail.com>
2022-02-06 22:58:07 +0200

    Added a command group of shooter and arc

---
+++ src/main/java/frc/robot/crossPlatforms/Commands.java
@@ -0,0 +1,18 @@
+package frc.robot.crossPlatforms;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.arc.Arc;
+import frc.robot.arc.commands.MoveArcToAngle;
+import frc.robot.shooter.Shooter;
+import frc.robot.shooter.commands.ShootByRPM;
+import java.util.function.DoubleSupplier;
+
+public class Commands extends ParallelCommandGroup {
+    public Commands(Arc arc, Shooter shooter, DoubleSupplier rpmSupplier , DoubleSupplier angleSupplier){
+        super(
+                new ShootByRPM(shooter,rpmSupplier),
+                new MoveArcToAngle(arc, angleSupplier)
+        );
+    }
+
+}
