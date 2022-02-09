809ea356b39f4c0d455ce2ce2ea9bcacd7b5a935
DanGoldenberg <yantegush@gmail.com>
2022-02-09 19:11:53 +0200

    UPDATE

--- src/main/java/frc/robot/vision/Vision.java
+++ src/main/java/frc/robot/vision/Vision.java
@@ -1,15 +1,20 @@
 package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.turret.Turret;
 import frc.robot.yawControll.YawControl;
 import vision.limelight.Limelight;
+import vision.limelight.enums.LimelightStreamMode;
 import vision.limelight.target.LimelightTarget;
 import static frc.robot.Constants.TARGET_X_RTF;
 import static frc.robot.Constants.TARGET_Y_RTF;
 import static frc.robot.vision.VisionConstants.*;
+import static vision.limelight.enums.LimelightOperationMode.driverCamera;
+import static vision.limelight.enums.LimelightStreamMode.pipMain;
+import static vision.limelight.enums.LimelightStreamMode.pipSecondary;
 
 
 public class Vision extends SubsystemBase {
@@ -21,7 +26,12 @@ public class Vision extends SubsystemBase {
     public Vision() {
         super();
         Shuffleboard.getTab("Vision").addNumber("Angle RTT", this::getHorizontalAngelTurretToTargetRTT);
+        Shuffleboard.getTab("Vision").addNumber("Distance", this::getHorizontalDistanceTurretToTarget);
         this.limelight = Limelight.getInstance();
+        limelight.setStreamMode(pipSecondary);
+        //limelight.setOperationMode(driverCamera);
+        //NetworkTableInstance.getDefault().getTable("limelight").getEntry("stream").setNumber(2);
+
     }
 
     @Override
@@ -46,7 +56,7 @@ public class Vision extends SubsystemBase {
             Vector2dEx limelightToTurret = Vector2dEx.fromMagnitudeDirection(LIMELIGHT_TO_TURRET_CM, 0);
             turretToTargetVectorRTT = Vector2dEx.fromMagnitudeDirection(getDistanceLimelightFromTarget(),
                     limelightOffsetFromTarget);
-            turretToTargetVectorRTT.add(limelightToTurret); //TODO: might be sub
+            turretToTargetVectorRTT.add(limelightToTurret); //TODO: it is in fact sub
         } else {
             turretToTargetVectorRTT = null;
         }
@@ -75,14 +85,26 @@ public class Vision extends SubsystemBase {
     }
 
     public double getRobotToTargetAngleRTF(YawControl yawControl) {
-        double horizontalAngelTurretToTargetRTT = getHorizontalAngelTurretToTargetRTT();
-        return horizontalAngelTurretToTargetRTT + yawControl.getTurretAngleRTF();
+        if (turretToTargetVectorRTT != null) {
+            double horizontalAngelTurretToTargetRTT = getHorizontalAngelTurretToTargetRTT();
+            return horizontalAngelTurretToTargetRTT + yawControl.getTurretAngleRTF();
+        }
+        return -999;
     }
 
     public Translation2d getXAndY(YawControl yawControl) {
-        double robotToTargetAngleRTF = getRobotToTargetAngleRTF(yawControl);
-        double x = TARGET_X_RTF - Math.cos(Math.toRadians(robotToTargetAngleRTF));
-        double y = TARGET_Y_RTF - Math.sin(Math.toRadians(robotToTargetAngleRTF));
-        return new Translation2d(x, y);
+            double robotToTargetAngleRTF = getRobotToTargetAngleRTF(yawControl);
+            double x = TARGET_X_RTF - Math.cos(Math.toRadians(robotToTargetAngleRTF));
+            double y = TARGET_Y_RTF - Math.sin(Math.toRadians(robotToTargetAngleRTF));
+            return new Translation2d(x, y);
+    }
+
+    public Translation2d getXAndYAuto(YawControl yawControl) {
+        if (hasTarget()) {
+            if (Math.abs(getHorizontalAngelTurretToTargetRTT()) < TURRET_TORRALANCE) {
+                return getXAndY(yawControl);
+            }
+        }
+        return new Translation2d(999,999);
     }
 }
--- src/main/java/frc/robot/vision/VisionConstants.java
+++ src/main/java/frc/robot/vision/VisionConstants.java
@@ -2,9 +2,10 @@ package frc.robot.vision;
 
 public final class VisionConstants {
 
-    public static final double OUTER_TARGET_HEIGHT_CM = 235;
+    public static final double OUTER_TARGET_HEIGHT_CM = 266;
     public static final double LIMELIGHT_HEIGHT_TO_FLOOR_CM = 59.3;
     public static final double LIMELIGHT_ANGLE_TO_HORIZON_DEG = 33;
     public static final double LIMELIGHT_TO_TARGET_CM = OUTER_TARGET_HEIGHT_CM - LIMELIGHT_HEIGHT_TO_FLOOR_CM;
-    public static final double LIMELIGHT_TO_TURRET_CM = 15.195728859;
+    public static final double LIMELIGHT_TO_TURRET_CM = 13;  //15.195728859
+    public static final double TURRET_TORRALANCE = 0.5;
 }
