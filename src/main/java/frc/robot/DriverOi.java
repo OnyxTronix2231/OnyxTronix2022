187faa60163482dbb833808ac99c8234ae630bb1
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-02-14 18:10:28 +0200

    yuval gur

--- src/main/java/frc/robot/DriverOi.java
+++ src/main/java/frc/robot/DriverOi.java
@@ -8,32 +8,33 @@ import frc.robot.trigger.BallTriggerOiBinder;
 import frc.robot.trigger.BallTrigger;
 import frc.robot.drivetrain.DriveTrain;
 import frc.robot.drivetrain.DriverDriveTrainOiBinders;
-import onyxTronix.JoystickAxis;
+import humanControls.ConsoleController;
+import humanControls.JoystickAxis;
 
 import static frc.robot.Constants.DRIVE_JOYSTICK_PORT;
 
 public class DriverOi {
 
-    final XboxController xboxController;
+    private ConsoleController consoleController;
 
     public DriverOi() {
-        xboxController = new XboxController(DRIVE_JOYSTICK_PORT);
+        consoleController = new ConsoleController(DRIVE_JOYSTICK_PORT);
     }
 
-    public DriverOi withDriveTrain(DriveTrain driveTrain) {
-        JoystickAxis leftJoystick = new JoystickAxis(xboxController, XboxController.Axis.kLeftY.value);
-        JoystickAxis rightJoystick = new JoystickAxis(xboxController, XboxController.Axis.kRightX.value);
+    public DriverOi withDriveTrain(DriveTrain driveTrain ) {
+        JoystickAxis leftJoystick = new JoystickAxis(consoleController, XboxController.Axis.kLeftY.value);
+        JoystickAxis rightJoystick = new JoystickAxis(consoleController, XboxController.Axis.kRightX.value);
         new DriverDriveTrainOiBinders(driveTrain, leftJoystick, rightJoystick);
         return this;
     }
 
     public DriverOi withTrigger(BallTrigger ballTrigger) {
-        JoystickButton triggerMoveBySpeed = new JoystickButton(xboxController, XboxController.Button.kA.value);
+        JoystickButton triggerMoveBySpeed = new JoystickButton(consoleController, XboxController.Button.kA.value);
         new BallTriggerOiBinder(ballTrigger, triggerMoveBySpeed);
         return this;
     }
     public DriverOi withLoader(Loader loader) {
-        JoystickButton loaderMoveBySpeed = new JoystickButton(xboxController, XboxController.Button.kY.value);
+        JoystickButton loaderMoveBySpeed = new JoystickButton(consoleController, XboxController.Button.kY.value);
         new LoaderOiBinder(loader, loaderMoveBySpeed);
         return this;
     }
--- src/main/java/frc/robot/drivetrain/DriverDriveTrainOiBinders.java
+++ src/main/java/frc/robot/drivetrain/DriverDriveTrainOiBinders.java
@@ -1,6 +1,7 @@
 package frc.robot.drivetrain;
 
 import frc.robot.drivetrain.commands.DriveBySpeed;
+import humanControls.JoystickAxis;
 import onyxTronix.JoystickAxis;
 
 public class DriverDriveTrainOiBinders {
@@ -8,4 +9,5 @@ public class DriverDriveTrainOiBinders {
     public DriverDriveTrainOiBinders(DriveTrain driveTrain, JoystickAxis leftJoystick, JoystickAxis rightJoystick) {
         driveTrain.setDefaultCommand(new DriveBySpeed(driveTrain, leftJoystick::getRawAxis, rightJoystick::getRawAxis));
     }
+
 }
