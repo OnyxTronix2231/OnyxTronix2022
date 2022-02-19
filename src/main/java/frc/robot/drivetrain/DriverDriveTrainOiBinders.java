f8b41e7b0487b6c889cc00740d453d6ea8077221
OfekNeuberger <75333158+OfekNeuberger@users.noreply.github.com>
2022-02-19 16:46:11 +0200

    added the second option path to the oi binders

--- src/main/java/frc/robot/drivetrain/DriverDriveTrainOiBinders.java
+++ src/main/java/frc/robot/drivetrain/DriverDriveTrainOiBinders.java
@@ -2,6 +2,7 @@ package frc.robot.drivetrain;
 
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.drivetrain.commands.DriveBySpeed;
+import frc.robot.drivetrain.commands.pathCommands.SecondPathOption;
 import frc.robot.drivetrain.commands.pathCommands.TestCommand;
 import humanControls.JoystickAxis;
 
@@ -10,5 +11,6 @@ public class DriverDriveTrainOiBinders {
     public DriverDriveTrainOiBinders(DriveTrain driveTrain, JoystickAxis leftJoystick, JoystickAxis rightJoystick, Trigger buttonA, Trigger buttonB) {
         driveTrain.setDefaultCommand(new DriveBySpeed(driveTrain, leftJoystick::getRawAxis, rightJoystick::getRawAxis));
         buttonA.whenActive(new TestCommand(driveTrain));
+        buttonB.whenActive(new SecondPathOption(driveTrain));
     }
 }
