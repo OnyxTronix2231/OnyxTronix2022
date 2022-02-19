00b589cb935f221248d325b0d8d86fda8ce3bc00
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-02-19 17:11:02 +0200

    fixed packges

--- src/main/java/frc/robot/conveyor/commands/TransferBallsToTrigger.java
+++ src/main/java/frc/robot/conveyor/commands/TransferBallsToTrigger.java
@@ -1,4 +1,4 @@
-package frc.robot.crossPlatform.teleopCommands;
+package frc.robot.conveyor.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
