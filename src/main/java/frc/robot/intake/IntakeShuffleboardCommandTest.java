34f359aaf0cf82a1eea0c35e1661cc43d987eb97
OfekNeuberger <ofekofek.n6655@gmail.com>
2022-02-19 22:10:38 +0200

    Update IntakeShuffleboardCommandTest.java
    WIP

--- src/main/java/frc/robot/intake/IntakeShuffleboardCommandTest.java
+++ src/main/java/frc/robot/intake/IntakeShuffleboardCommandTest.java
@@ -9,18 +9,18 @@ import frc.robot.intake.commands.OpenPiston;
 public class IntakeShuffleboardCommandTest {
 
     public IntakeShuffleboardCommandTest(Intake intakeFront, Intake intakeBack) {
-        Shuffleboard.getTab("intake").add(new OpenPiston(intakeFront));
-        Shuffleboard.getTab("intake").add(new OpenPiston(intakeBack));
+        Shuffleboard.getTab("intake").add("open front", new OpenPiston(intakeFront));
+        Shuffleboard.getTab("intake").add("open back", new OpenPiston(intakeBack));
 
-        Shuffleboard.getTab("intake").add(new ClosePiston(intakeFront));
-        Shuffleboard.getTab("intake").add(new ClosePiston(intakeBack));
+        Shuffleboard.getTab("intake").add("close front", new ClosePiston(intakeFront));
+        Shuffleboard.getTab("intake").add("close back", new ClosePiston(intakeBack));
 
         var nimiFront  = Shuffleboard.getTab("intake").add("front-intake-speed",
                 0.0).getEntry();
-        Shuffleboard.getTab("intake").add(new IntakeBySpeed(intakeFront,()->nimiFront.getDouble(0)));
+        Shuffleboard.getTab("intake").add("intake front speed", new IntakeBySpeed( intakeFront,()->nimiFront.getDouble(0)));
 
         var nimiBack  = Shuffleboard.getTab("intake").add("back-intake-speed",
                 0.0).getEntry();
-        Shuffleboard.getTab("intake").add(new IntakeBySpeed(intakeBack,()->nimiBack.getDouble(0)));
+        Shuffleboard.getTab("intake").add("intake back speed", new IntakeBySpeed(intakeBack,()->nimiBack.getDouble(0)));
     }
 }
