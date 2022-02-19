64a24d1305942abec2b43549fe859f8915e76118
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-02-19 21:53:41 +0200

    fix rev2m sensor libray

--- src/main/java/frc/robot/intake/IntakeOiBinder.java
+++ src/main/java/frc/robot/intake/IntakeOiBinder.java
@@ -1,19 +1,19 @@
-package frc.robot.intake;
-
-import frc.robot.drivetrain.DriveTrain;
-import frc.robot.intake.commands.IntakeByDriveTrain;
-import humanControls.JoystickAxis;
-
-import java.util.function.DoubleSupplier;
-
-import static frc.robot.intake.IntakeConstant.INTAKE_SPEED;
-
-public class IntakeOiBinder {
-
-    public IntakeOiBinder(DriveTrain driveTrain, Intake intakeForward, Intake intakeBack,
-                          JoystickAxis openAndIntakeTwoSides) {
-        openAndIntakeTwoSides.whileActiveContinuous(new IntakeByDriveTrain(intakeForward, intakeBack,
-                () -> INTAKE_SPEED, driveTrain));
-    }
-}
-
+//package frc.robot.intake;
+//
+//import frc.robot.drivetrain.DriveTrain;
+//import frc.robot.intake.commands.IntakeByDriveTrain;
+//import humanControls.JoystickAxis;
+//
+//import java.util.function.DoubleSupplier;
+//
+//import static frc.robot.intake.IntakeConstant.INTAKE_SPEED;
+//
+//public class IntakeOiBinder {
+//
+//    public IntakeOiBinder(DriveTrain driveTrain, Intake intakeForward, Intake intakeBack,
+//                          JoystickAxis openAndIntakeTwoSides) {
+//        openAndIntakeTwoSides.whileActiveContinuous(new IntakeByDriveTrain(intakeForward, intakeBack,
+//                () -> INTAKE_SPEED, driveTrain));
+//    }
+//}
+//
