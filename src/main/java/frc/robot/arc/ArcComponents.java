76da7f661bb7fdc222a1e0d62b08c54498cba6b9
EladSegev1 <94128681+EladSegev1@users.noreply.github.com>
2022-02-27 19:45:32 +0200

    added micro switch

--- src/main/java/frc/robot/arc/ArcComponents.java
+++ src/main/java/frc/robot/arc/ArcComponents.java
@@ -1,8 +1,12 @@
 package frc.robot.arc;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import sensors.Switch.TalonSrxForwardMicroswitch;
+import sensors.Switch.TalonSrxReverseMicroswitch;
 
 public interface ArcComponents {
 
     WPI_TalonSRX getMotor();
+    TalonSrxForwardMicroswitch getForwardMicroSwitch();
+    TalonSrxReverseMicroswitch getReverseMicroSwitch();
 }
--- src/main/java/frc/robot/arc/ArcComponentsBase.java
+++ src/main/java/frc/robot/arc/ArcComponentsBase.java
@@ -1,6 +1,11 @@
 package frc.robot.arc;
 
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import sensors.Switch.Microswitch;
+import sensors.Switch.TalonSrxForwardMicroswitch;
+import sensors.Switch.TalonSrxReverseMicroswitch;
 import sensors.counter.TalonEncoder;
 
 import static frc.robot.arc.ArcConstants.ComponentsConstants.*;
@@ -10,14 +15,29 @@ public class ArcComponentsBase implements ArcComponents {
 
     private final WPI_TalonSRX motor;
     private final TalonEncoder encoder;
+    private final TalonSrxForwardMicroswitch forwardMicroswitch;
+    private final TalonSrxReverseMicroswitch reverseMicroswitch;
 
     public ArcComponentsBase() {
         motor = new WPI_TalonSRX(MOTOR_ID);
         motor.configFactoryDefault();
 
         encoder = new TalonEncoder(motor);
-    }
 
+        forwardMicroswitch = new TalonSrxForwardMicroswitch(motor, LimitSwitchSource.FeedbackConnector,
+                LimitSwitchNormal.NormallyOpen);
+        reverseMicroswitch = new TalonSrxReverseMicroswitch(motor, LimitSwitchSource.FeedbackConnector,
+                LimitSwitchNormal.NormallyOpen);
+    }
+    
+    @Override
+    public TalonSrxReverseMicroswitch getReverseMicroswitch(){
+        return reverseMicroswitch;
+    }
+    @Override
+    public TalonSrxForwardMicroswitch getForwardMicroswitch(){
+        return forwardMicroswitch;
+    }
     @Override
     public WPI_TalonSRX getMotor() {
         return motor;
