5e2ff65fea464edb745d677fac907106b1fbaf06
benalaluf <iben.alaluf@gmail.com>
2022-02-27 22:47:24 +0200

    refactor

--- src/main/java/frc/robot/climber/ClimberComponents.java
+++ src/main/java/frc/robot/climber/ClimberComponents.java
@@ -1,9 +1,7 @@
 package frc.robot.climber;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.sensors.WPI_PigeonIMU;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Solenoid;
 import sensors.counter.TalonEncoder;
 
 public interface ClimberComponents {
