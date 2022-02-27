6e7003c1c7cc9115dbf3a9a108a7d6eefcfe802d
shaharbetser <shaharbetser8@gmail.com>
2022-02-27 20:47:55 +0200

    CR

--- src/main/java/frc/robot/turret/commands/RotateByAngleConstantly.java
+++ src/main/java/frc/robot/turret/commands/RotateByAngleConstantly.java
@@ -1,5 +1,13 @@
 package frc.robot.turret.commands;
 
-public class RotateByAngleConstantly {
-    
+import frc.robot.turret.Turret;
+
+import java.util.function.DoubleSupplier;
+
+public class RotateByAngleConstantly extends RotateToAngleRTR {
+
+    public RotateByAngleConstantly(Turret turret, DoubleSupplier angleSupplier) {
+        super(turret, () -> (angleSupplier.getAsDouble() + turret.getCurrentAngleRTR()));
+
+    }
 }
