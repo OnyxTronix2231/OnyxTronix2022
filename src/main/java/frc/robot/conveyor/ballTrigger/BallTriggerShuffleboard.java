e12f4c90715157ebfb4a9a42d916cba9ea988cac
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-03-03 18:55:18 +0200

    added analgo distance sensor to ballTrigger

--- src/main/java/frc/robot/conveyor/ballTrigger/BallTriggerShuffleboard.java
+++ src/main/java/frc/robot/conveyor/ballTrigger/BallTriggerShuffleboard.java
@@ -27,7 +27,8 @@ public class BallTriggerShuffleboard {
         Shuffleboard.getTab("BallTrigger").add(new MoveBallTriggerBySpeed(ballTrigger,
                 () -> ballTriggerSpeed.getDouble(BALLTRIGGER_SPEED)));
 
-        Shuffleboard.getTab("BallTrigger").addNumber("Distance", ballTrigger::getDistance);
+        Shuffleboard.getTab("BallTrigger").addNumber("DistanceDigital", ballTrigger::getDigitalDistance);
+        Shuffleboard.getTab("BallTrigger").addNumber("DistanceAnalog", ballTrigger::getAnalogDistance);
         Shuffleboard.getTab("BallTrigger").addBoolean("IsBallIdentify", ballTrigger::isBallIdentified);
         Shuffleboard.getTab("BallTrigger").addBoolean("IsBallInPlace", ballTrigger::isBallInPlace);
     }
