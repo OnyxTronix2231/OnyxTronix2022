60dee3e2e88c0dda1d9124e89f7140fab535e46e
YuvalGur135 <yuval.gur135@gmail.com>
2022-03-04 20:54:34 +0200

    calibrated shooter

--- src/main/java/frc/robot/shooter/ShooterComponentsBase.java
+++ src/main/java/frc/robot/shooter/ShooterComponentsBase.java
@@ -1,5 +1,6 @@
 package frc.robot.shooter;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import pid.CtrePIDController;
 import pid.PIDControlMode;
@@ -8,9 +9,11 @@ import pid.interfaces.PIDController;
 import sensors.counter.Counter;
 import sensors.counter.TalonEncoder;
 
+import static frc.robot.conveyor.loader.LoaderConstants.ComponentsConstant.*;
 import static frc.robot.shooter.ShooterConstants.*;
 import static frc.robot.shooter.ShooterConstants.ComponentsConstants.MASTER_MOTOR_ID;
 import static frc.robot.shooter.ShooterConstants.ComponentsConstants.SLAVE_MOTOR_ID;
+import static frc.robot.shooter.ShooterConstants.ShooterCalculations.RPMToEncUnitsDecisec;
 
 public class ShooterComponentsBase implements ShooterComponents{
 
@@ -22,9 +25,11 @@ public class ShooterComponentsBase implements ShooterComponents{
     public ShooterComponentsBase() {
         masterMotor = new WPI_TalonFX(MASTER_MOTOR_ID);
         masterMotor.configFactoryDefault();
+        masterMotor.configAllSettings(getConfiguration());
 
         slaveMotor = new WPI_TalonFX(SLAVE_MOTOR_ID);
         slaveMotor.configFactoryDefault();
+        slaveMotor.configAllSettings(getConfiguration());
         slaveMotor.follow(masterMotor);
         slaveMotor.setInverted(true);
 
@@ -35,6 +40,21 @@ public class ShooterComponentsBase implements ShooterComponents{
         controller.setPIDFTerms(controller.getPIDFTerms());
     }
 
+    public TalonFXConfiguration getConfiguration(){
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.closedloopRamp = CLOSE_LOOP_RAMP;
+        config.slot0.integralZone = RPMToEncUnitsDecisec(INTEGRAL_ZONE);
+        config.supplyCurrLimit.currentLimit = SUPPLY_CURRENT_LIMIT;
+        config.supplyCurrLimit.triggerThresholdCurrent = SUPPLY_TRIGGER_THRESHOLD_CURRENT;
+        config.supplyCurrLimit.triggerThresholdTime = SUPPLY_TRIGGER_THRESHOLD_TIME;
+        config.supplyCurrLimit.enable = SUPPLY_CURRENT_LIMIT_ENABLED;
+        config.statorCurrLimit.currentLimit = STATOR_CURRENT_LIMIT;
+        config.statorCurrLimit.triggerThresholdCurrent = STATOR_TRIGGER_THRESHOLD_CURRENT;
+        config.statorCurrLimit.triggerThresholdTime = STATOR_TRIGGER_THRESHOLD_TIME;
+        config.statorCurrLimit.enable = STATOR_CURRENT_LIMIT_ENABLED;
+        config.openloopRamp = OPEN_LOOP_RAMP;
+        return config;
+    }
     @Override
     public WPI_TalonFX getMasterMotor() {
         return masterMotor;
