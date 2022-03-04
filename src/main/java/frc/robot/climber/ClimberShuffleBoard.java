623844849d226df6d1368a4b5fab9cf0cbc2f632
ofir6868 <ofiregev68@gmail.com>
2022-03-04 19:58:30 +0200

    added shuffleboard value

--- src/main/java/frc/robot/climber/ClimberShuffleBoard.java
+++ src/main/java/frc/robot/climber/ClimberShuffleBoard.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import frc.robot.climber.commands.ClimbBySpeed;
 import frc.robot.climber.commands.ClosePistons;
 import frc.robot.climber.commands.OpenPistons;
+import frc.robot.climber.commands.WaitUntilEncoderPosition;
 
 import static frc.robot.climber.ClimberConstants.CLOSE_CLIMBER_SPEED;
 import static frc.robot.climber.ClimberConstants.OPEN_CLIMBER_SPEED;
@@ -17,6 +18,8 @@ public class ClimberShuffleBoard {
 
         Shuffleboard.getTab("Climber").addNumber("leftEncoderUnits", climber::getLeftEncoderUnits);
         Shuffleboard.getTab("Climber").addNumber("rightEncoderUnits", climber::getRightEncoderUnits);
+        Shuffleboard.getTab("Climber").addBoolean("climber reached fourth bar",
+                () -> (climber.isLeftEncoderOnTarget() && climber.isRightEncoderOnTarget()));
 
         setSpeed = Shuffleboard.getTab("Climber").add("setSpeed", 0).getEntry();
 
@@ -28,5 +31,6 @@ public class ClimberShuffleBoard {
                 new ClimbBySpeed(climber, () -> OPEN_CLIMBER_SPEED));
         Shuffleboard.getTab("Climber").add("closeClimberArms",
                 new ClimbBySpeed(climber, () -> CLOSE_CLIMBER_SPEED));
+
     }
 }
