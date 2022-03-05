d6c4419cfacc9885d1562387c2ca093e557c7c2c
ofir6868 <ofiregev68@gmail.com>
2022-03-05 10:25:07 +0200

    Fixed conventions

--- src/main/java/frc/robot/advancedClimber/AdvancedClimberShuffleBoard.java
+++ src/main/java/frc/robot/advancedClimber/AdvancedClimberShuffleBoard.java
@@ -3,10 +3,11 @@ package frc.robot.advancedClimber;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 public class AdvancedClimberShuffleBoard {
+
     public AdvancedClimberShuffleBoard(AdvancedClimber advancedClimber) {
+
         Shuffleboard.getTab("CrossPlatform").addNumber("current pitch", advancedClimber::getCurrentPitch);
         Shuffleboard.getTab("CrossPlatform").addBoolean("is on desired angle and encoder position",
                 advancedClimber::hasReachedFourthBar);
     }
-
 }
