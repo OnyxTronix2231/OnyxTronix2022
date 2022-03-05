d88bf7a81d94af2b094cfdec91435abe84e0f747
ofir6868 <ofiregev68@gmail.com>
2022-03-05 17:15:04 +0200

    changed name

--- src/main/java/frc/robot/advancedClimber/commands/ClimbUntilConditions.java
+++ src/main/java/frc/robot/advancedClimber/commands/ClimbUntilConditions.java
@@ -4,14 +4,14 @@ import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.advancedClimber.AdvancedClimber;
 import frc.robot.climber.Climber;
 import frc.robot.climber.commands.ClimbBySpeed;
-import frc.robot.climber.commands.WaitUntilEncoderPosition;
+import frc.robot.climber.commands.WaitUntilClimbedByEncoder;
 
 import java.util.function.DoubleSupplier;
 
 public class ClimbUntilConditions extends ParallelDeadlineGroup {
 
     public ClimbUntilConditions(Climber climber, AdvancedClimber advancedClimber, DoubleSupplier speedSupplier){
-        super(new WaitUntilEncoderPosition(climber).alongWith(new WaitUntilRobotPitchAngle(advancedClimber)),
+        super(new WaitUntilClimbedByEncoder(climber).alongWith(new WaitUntilClimbedByPitch(advancedClimber)),
                 new ClimbBySpeed(climber, speedSupplier));
     }
 }
