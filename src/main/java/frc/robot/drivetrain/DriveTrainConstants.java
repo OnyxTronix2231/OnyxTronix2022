60545f03edb99df1e69f2333e211f387c8fd6d2e
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-03-06 00:03:42 +0200

    chnage RAMP_TIME

--- src/main/java/frc/robot/drivetrain/DriveTrainConstants.java
+++ src/main/java/frc/robot/drivetrain/DriveTrainConstants.java
@@ -38,7 +38,7 @@ public final class DriveTrainConstants {
         static final double CURRENT_LIMIT = 40;
         static final double TRIGGER_THRESHOLD_CURRENT = 0;
         static final double TRIGGER_THRESHOLD_TIME = 0;
-        static final double RAMP_TIME = 0.3;
+        static final double RAMP_TIME = 0.4;
     }
 
     public static final class Calculations {
