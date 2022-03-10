7a5a6122b5f3377ba25718a64b69384c649447ff
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-03-10 16:27:10 +0200

    Update DeputyOi.java

--- src/main/java/frc/robot/DeputyOi.java
+++ src/main/java/frc/robot/DeputyOi.java
@@ -2,8 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.climber.Climber;
-import frc.robot.climber.DeputeClimberOiBinder;
 import frc.robot.arc.Arc;
 import frc.robot.arc.CalibrateArcOiBinder;
 import frc.robot.conveyor.ballTrigger.BallTrigger;
@@ -30,17 +28,6 @@ public class DeputyOi {
 
     public DeputyOi() {
         controller = new OnyxXboxController(DEPUTY_JOYSTICK_PORT);
-    }
-
-    public DeputyOi withClimber(Climber climber){
-        JoystickAxis climb = new JoystickAxis(controller, controller.getAxisLeftY());
-        Trigger open = new JoystickButton(controller, controller.getButtonUp());
-        Trigger down = new JoystickButton(controller, controller.getButtonDown());
-        Trigger moveRightArmSlow = new JoystickButton(controller, controller.getButtonRight());
-        Trigger moveLeftArmSlow = new JoystickButton(controller, controller.getButtonLeft());
-        JoystickAxis moveLeftArm = new JoystickAxis(controller, controller.getAxisRightY());
-        new DeputeClimberOiBinder(climber, down, open, climb, moveRightArmSlow, moveLeftArmSlow, moveLeftArm);
-        return this;
     }
 
     public DeputyOi withGetReadyToShoot(Shooter shooter, Arc arc, YawControl yawControl,
