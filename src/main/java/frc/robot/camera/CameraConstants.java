83a3d0fdb5e6a2f2959bcc27cfebe83b736f5ffc
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-03-10 16:33:53 +0200

    delete camera

--- src/main/java/frc/robot/camera/CameraConstants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.camera;
-
-public final class CameraConstants {
-
-    static final int WIDTH = 88;
-    static final int HEIGHT = 60;
-    static final int FPS = 30;
-    static final int EXPOSURE = 60;
-}
