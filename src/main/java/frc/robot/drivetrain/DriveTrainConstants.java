f164aa5e197b5ca0bd329c324d7476841a3fd4fc
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2022-03-10 16:35:34 +0200

    reset driveTrainConstants

--- src/main/java/frc/robot/drivetrain/DriveTrainConstants.java
+++ src/main/java/frc/robot/drivetrain/DriveTrainConstants.java
@@ -27,10 +27,10 @@ public final class DriveTrainConstants {
 
     public static final class ComponentsConstants {
 
-        static final int RIGHT_MASTER_MOTOR_PORT = 3;
-        static final int RIGHT_SLAVE_MOTOR_PORT = 4;
-        static final int LEFT_MASTER_MOTOR_PORT = 1;
-        static final int LEFT_SLAVE_MOTOR_PORT = 2;
+        static final int RIGHT_MASTER_MOTOR_PORT = 1;
+        static final int RIGHT_SLAVE_MOTOR_PORT = 2;
+        static final int LEFT_MASTER_MOTOR_PORT = 3;
+        static final int LEFT_SLAVE_MOTOR_PORT = 4;
         static final int PIGEON_PORT = 0;
 
         static final double MAX_OUTPUT_FORWARD = 1;
