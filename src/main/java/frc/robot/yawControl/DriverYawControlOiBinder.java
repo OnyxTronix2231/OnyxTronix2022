665d0fcc3a64e41574c9e352e34eae092e282a37
YuvalGur135 <yuval.gur135@gmail.com>
2022-03-12 13:48:55 +0200

    CR

--- src/main/java/frc/robot/yawControl/DriverYawControlOiBinder.java
+++ src/main/java/frc/robot/yawControl/DriverYawControlOiBinder.java
@@ -1,20 +1,11 @@
 package frc.robot.yawControl;
 
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.turret.commands.RotateBySpeed;
 import frc.robot.turret.commands.RotateToAngleRTR;
-import frc.robot.vision.Vision;
-import frc.robot.yawControl.Commands.RotateToAngleRTF;
-
-import static frc.robot.turret.TurretConstants.DEFAULT_TURRET_SPEED;
-import static frc.robot.turret.TurretConstants.MIDDLE_ANGLE;
 
 public class DriverYawControlOiBinder {
 
-    public DriverYawControlOiBinder(YawControl yawControl, Trigger rotateByPose, Trigger moveLeft, Trigger moveRight, Vision vision) {
+    public DriverYawControlOiBinder(YawControl yawControl, Trigger rotateByPose) {
         rotateByPose.whileActiveContinuous(new RotateToAngleRTR(yawControl, yawControl::getAngleRTRToTarget));
-        moveLeft.whileActiveContinuous(new RotateBySpeed(yawControl, () -> DEFAULT_TURRET_SPEED));
-        moveRight.whileActiveContinuous(new RotateBySpeed(yawControl, () -> -DEFAULT_TURRET_SPEED));
     }
 }
