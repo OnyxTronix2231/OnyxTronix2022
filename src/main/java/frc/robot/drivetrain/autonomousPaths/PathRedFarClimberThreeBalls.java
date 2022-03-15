63eea9371f1c5c3591233b008afee5ffd8509560
YuvalGur135 <yuval.gur135@gmail.com>
2022-03-15 23:10:44 +0200

    cr13

--- src/main/java/frc/robot/drivetrain/autonomousPaths/PathRedFarClimberThreeBalls.java
+++ src/main/java/frc/robot/drivetrain/autonomousPaths/PathRedFarClimberThreeBalls.java
@@ -12,16 +12,10 @@ import java.util.List;
 public class PathRedFarClimberThreeBalls implements AutonomousPaths{
 
     static final double X_START_POSE = 8.943985451508386;
-    static final double X_SECOND_BALL = 10.968;
+    static final double X_SECOND_BALL = 11.126;
 
     static final double Y_START_POSE = 6.15;
-    static final double Y_SECOND_BALL = 6.374;
-
-    static final double START_DEGREE = -90;
-    static final double SPOT_DEGREE = 45;
-
-    static final double MAX_VELOCITY_METERS_PER_SECOND = 2;
-    static final double MAX_CENTRIPETAL_ACCELERATION_METERS_PER_SECOND_SQ = 2;
+    static final double Y_SECOND_BALL = 6.408;
 
     private final Pose2d startPose;
     private final Path firstPath;
@@ -30,13 +24,13 @@ public class PathRedFarClimberThreeBalls implements AutonomousPaths{
 
     public PathRedFarClimberThreeBalls(){
         startPose = new Pose2d(X_START_POSE, Y_START_POSE,
-                Rotation2d.fromDegrees(START_DEGREE));
+                Rotation2d.fromDegrees(-90));
         firstPath = new Path(
                 List.of(),
-                new Pose2d(X_SECOND_BALL, Y_SECOND_BALL, Rotation2d.fromDegrees(SPOT_DEGREE)),
-                new MaxVelocityConstraint(MAX_VELOCITY_METERS_PER_SECOND),
-                new CentripetalAccelerationConstraint(MAX_CENTRIPETAL_ACCELERATION_METERS_PER_SECOND_SQ)
-        ).setReversed();
+                new Pose2d(X_SECOND_BALL, Y_SECOND_BALL, Rotation2d.fromDegrees(0)),
+                new MaxVelocityConstraint(2),
+                new CentripetalAccelerationConstraint(2)
+        );
         paths.add(firstPath);
     }
     @Override
