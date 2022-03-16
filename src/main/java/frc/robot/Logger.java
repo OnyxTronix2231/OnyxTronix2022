8f56d71818d9914bb7b6f0cd861e50a2127478d6
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-03-16 15:51:15 +0200

    add doubleFollower

--- src/main/java/frc/robot/Logger.java
+++ src/main/java/frc/robot/Logger.java
@@ -2,14 +2,17 @@ package frc.robot;
 
 import java.util.ArrayList;
 import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
 
 public class Logger {
 
     private final String tag;
     ArrayList<BooleanFollower> booleanFollowers;
+    ArrayList<DoubleFollower> doubleFollowers;
 
     public Logger(String tag) {
         booleanFollowers = new ArrayList<>();
+        doubleFollowers = new ArrayList<>();
         this.tag = tag;
     }
 
@@ -23,13 +26,30 @@ public class Logger {
             if (booleanFollower.getValue() != booleanFollower.getLastValue()) {
                 System.out.println(tag + "was changed toz");
                 booleanFollower.setLastValue();
+            }
+        }
+    }
 
+    public void updateDoubleFollower() {
+
+        for (var doubleFollower :
+                doubleFollowers) {
+            if (doubleFollower.getValue() > doubleFollower.getLastValue() + doubleFollower.getTolerance() ||
+                    doubleFollower.getValue() < doubleFollower.getLastValue() - doubleFollower.getTolerance()) {
+
+                System.out.println(tag + "setPoint was changed");
+                doubleFollower.setLastValue();
             }
         }
     }
 
     public void addBooleanListener(String methodName, BooleanSupplier condition, int miliSec) {
-        BooleanFollower booleanFollower = new BooleanFollower(methodName, condition, condition.getAsBoolean(),convertMiliSecondsToUnits(miliSec));
+        BooleanFollower booleanFollower = new BooleanFollower(methodName, condition, condition.getAsBoolean(),
+                convertMiliSecondsToUnits(miliSec));
+    }
+
+    public void addDoubleListener(String methodName, DoubleSupplier setPoint, int delay, int tolerace) {
+        DoubleFollower doubleFollower = new DoubleFollower(methodName, setPoint, delay, tolerace);
     }
 
     public class BooleanFollower {
@@ -39,7 +59,8 @@ public class Logger {
         private boolean lastCondition;
         private int updateInterval;
 
-        public BooleanFollower(String name, BooleanSupplier booleanSupplier, boolean lastCondition, int updateInterval) {
+        public BooleanFollower(
+                String name, BooleanSupplier booleanSupplier, boolean lastCondition, int updateInterval) {
             this.name = name;
             this.updateInterval = updateInterval;
             this.booleanSupplier = booleanSupplier;
@@ -62,8 +83,49 @@ public class Logger {
             return name;
         }
 
-        public int getCounter(){
+        public int getCounter() {
             return updateInterval;
         }
+    }
+
+    public class DoubleFollower {
+
+        private final String name;
+        private final DoubleSupplier doubleSupplier;
+        private double lastValue;
+        private int updateInterval;
+        private double tolerance;
+
+        public DoubleFollower(String name, DoubleSupplier doubleSupplier, int updateInterval, double tolerance) {
+            this.name = name;
+            this.updateInterval = updateInterval;
+            this.doubleSupplier = doubleSupplier;
+            this.tolerance = tolerance;
+        }
+
+        public double getValue() {
+            return doubleSupplier.getAsDouble();
+        }
+
+        public double getLastValue() {
+            return lastValue;
+        }
+
+        public void setLastValue() {
+            lastValue = doubleSupplier.getAsDouble();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getCounter() {
+            return updateInterval;
+        }
+
+        public double getTolerance() {
+            return tolerance;
+        }
+
     }
 }
