e1333368344288d57331b211f2478a517873eaaf
benalaluf <94129183+benalaluf@users.noreply.github.com>
2022-03-16 16:15:03 +0200

    Update Logging.java

--- src/main/java/frc/robot/LoggingTest/Logging.java
+++ src/main/java/frc/robot/LoggingTest/Logging.java
@@ -4,12 +4,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Logger;
 
 public class Logging extends SubsystemBase {
-     private final LoggingCompoents compoents;
+     private final LoggingCompoents components;
      private final Logging logging;
      private final Logger log;
 
-    public Logging(LoggingCompoents compoents, Logging logging, Logger log) {
-        this.compoents = compoents;
+    public Logging(LoggingCompoents components, Logging logging, Logger log) {
+        this.components = components;
         this.logging = logging;
         this.log = new Logger("loader");
         this.log.addBooleanListener("test", ()-> isOpen(), 20);
@@ -17,6 +17,6 @@ public class Logging extends SubsystemBase {
     }
 
     public boolean isOpen(){
-        return compoents.getMicroSwitch().isOpen();
+        return components.getMicroSwitch().isOpen();
     }
 }
