e8b420cb3c9cc6512d04b45146b43c59a7808739
killery2201 <nivyuval11@gmail.com>
2022-03-19 17:00:49 +0200

    Added ClimberTesting

--- src/main/java/frc/robot/arms/ArmsShuffleBoard.java
+++ src/main/java/frc/robot/arms/ArmsShuffleBoard.java
@@ -6,30 +6,25 @@ import frc.robot.arms.commands.MoveArmsBySpeed;
 
 public class ArmsShuffleBoard {
 
-    private final Arms climber;
+    private final Arms arms;
     private NetworkTableEntry setSpeed;
     private NetworkTableEntry setSpeedLeft;
     private NetworkTableEntry setSpeedRight;
 
-    public ArmsShuffleBoard(Arms climber) {
-        this.climber = climber;
+    public ArmsShuffleBoard(Arms arms) {
+        this.arms = arms;
+        init();
     }
 
     public void init() {
-        /*Shuffleboard.getTab("Climber").addNumber("leftEncoderUnits", climber::getLeftEncoderUnits);
-        Shuffleboard.getTab("Climber").addNumber("rightEncoderUnits", climber::getRightEncoderUnits);
-        Shuffleboard.getTab("Climber").addBoolean("climber reached fourth bar",
-                () -> (climber.isLeftEncoderOnTarget() && climber.isRightEncoderOnTarget()));*/
+        Shuffleboard.getTab("Climber").addNumber("leftEncoderUnits", arms::getArmsEncoderUnits);
 
-        setSpeed = Shuffleboard.getTab("Climber").add("setSpeed", 0).getEntry();
-        setSpeedLeft = Shuffleboard.getTab("Climber").add("setSpeedLeft", 0).getEntry();
-        setSpeedRight = Shuffleboard.getTab("Climber").add("setSpeedRight", 0).getEntry();
+        setSpeed = Shuffleboard.getTab("Climber").add("set arms speed", 0).getEntry();
 
         //Shuffleboard.getTab("Climber").add("openPiston", new OpenPistons(climber));
         //Shuffleboard.getTab("Climber").add("closePiston", new ClosePistons(climber));
 
-
-        Shuffleboard.getTab("Climber").add("Move Arms by Speed", new MoveArmsBySpeed(climber,
+        Shuffleboard.getTab("Climber").add("Move Arms by Speed", new MoveArmsBySpeed(arms,
                 () -> setSpeedLeft.getDouble(0)));
 
     }
---
+++ src/main/java/frc/robot/stabilizers/commands/StabilizerShuffleBoard.java
@@ -0,0 +1,22 @@
+package frc.robot.stabilizers.commands;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import frc.robot.arms.Arms;
+import frc.robot.arms.commands.MoveArmsBySpeed;
+import frc.robot.stabilizers.Stabilizers;
+
+public class StabilizerShuffleBoard {
+    private final Stabilizers stabilizer;
+    private NetworkTableEntry setSpeed;
+    private NetworkTableEntry setSpeedLeft;
+    private NetworkTableEntry setSpeedRight;
+
+    public StabilizerShuffleBoard(Stabilizers stabilizer) {
+        this.stabilizer = stabilizer;
+    }
+
+    public void init() {
+        Shuffleboard.getTab("Climber").addNumber("Stabilizer encoder units", stabilizer::getEncoderUnits);
+    }
+}
