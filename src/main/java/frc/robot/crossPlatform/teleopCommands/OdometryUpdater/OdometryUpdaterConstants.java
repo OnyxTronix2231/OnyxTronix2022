4bcde6abf72e9df1ae16f4aa18d7bc5b6ecdafef
YuvalGur135 <yuval.gur135@gmail.com>
2022-03-23 15:44:54 +0200

    76irtuoklp'[;

--- src/main/java/frc/robot/crossPlatform/teleopCommands/OdometryUpdater/OdometryUpdaterConstants.java
+++ src/main/java/frc/robot/crossPlatform/teleopCommands/OdometryUpdater/OdometryUpdaterConstants.java
@@ -3,5 +3,5 @@ package frc.robot.crossPlatform.teleopCommands.OdometryUpdater;
 public final class OdometryUpdaterConstants {
 
     static final double ODOMETRY_UPDATE_PERIOD = 0.3;
-    static final double FIXABLE_ANGLE_OFFSET = 50;
+    static final double FIXABLE_ANGLE_OFFSET = 5;
 }
