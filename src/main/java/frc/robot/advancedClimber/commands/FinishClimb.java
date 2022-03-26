08daca9dda0f8cc06156b7a0221e444901de58a1
YuvalGur135 <yuval.gur135@gmail.com>
2022-03-26 23:39:51 +0300

    Changed finishClimb to open loop

--- src/main/java/frc/robot/advancedClimber/commands/FinishClimb.java
+++ src/main/java/frc/robot/advancedClimber/commands/FinishClimb.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.advancedClimber.AdvancedClimber;
 import frc.robot.arms.Arms;
 import frc.robot.arms.commands.MoveArmsBySpeed;
+import frc.robot.stabilizers.commands.MoveStabilizerBySpeed;
 import frc.robot.stabilizers.commands.MoveStabilizerUntilOnDeltaPosition;
 import frc.robot.stabilizers.commands.WaitUntilStabilizersOnDeltaPositionByEncoder;
 
@@ -15,7 +16,7 @@ import static frc.robot.advancedClimber.AdvancedClimberConstants.*;
 public class FinishClimb extends ParallelCommandGroup {
 
         public FinishClimb(AdvancedClimber advancedClimber, Arms arms, IntSupplier deltaForStabilizerFinish) {
-            super(new MoveStabilizerUntilOnDeltaPosition(advancedClimber, () -> DESIRED_STABILIZER_SPEED_STAGE_FOUR, deltaForStabilizerFinish),
+            super(new MoveStabilizerBySpeed(advancedClimber, () -> DESIRED_STABILIZER_SPEED_STAGE_FOUR).withTimeout(1),
                     new MoveArmsBySpeed(arms, () -> DESIRED_ARMS_SPEED_STAGE_THREE).withTimeout(0.5));
         }
     }
